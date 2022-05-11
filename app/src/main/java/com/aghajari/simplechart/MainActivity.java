2f9b9be41658a77934c4b83fb3bb37ef7fb409bd
Aghajari <amir.aghajari@aut.ac.ir>
2022-05-11 21:15:17 +0430

    LineChart2 added

--- app/src/main/java/com/aghajari/simplechart/MainActivity.java
+++ app/src/main/java/com/aghajari/simplechart/MainActivity.java
@@ -20,10 +20,15 @@ public class MainActivity extends AppCompatActivity {
         lineChart.setPointColor(ResourcesCompat.getColor(getResources(), R.color.teal_700, null));
         lineChart.addData(8f, 10f, 5f, 7f, 4f, 6f);
 
+        LineChart2 lineChart2 = findViewById(R.id.line_chart2);
+        lineChart2.setLineColor(ResourcesCompat.getColor(getResources(), R.color.second, null));
+        lineChart2.setPointColor(ResourcesCompat.getColor(getResources(), R.color.teal_700, null));
+        lineChart2.addData(8f, 10f, 5f, 7f, 4f, 6f);
+
         CircleChart circleChart = findViewById(R.id.circle_chart);
-        circleChart.addData(0xFF7AA6E8, Color.WHITE, 150, 40);
-        circleChart.addData(0xFFD273A2, Color.WHITE, 125, 35);
-        circleChart.addData(0xFFE2B263, Color.WHITE, 100, 25);
+        circleChart.addData(0xFF7AA6E8, Color.WHITE, 170, 40);
+        circleChart.addData(0xFFD273A2, Color.WHITE, 150, 35);
+        circleChart.addData(0xFFE2B263, Color.WHITE, 130, 25);
 
         BarChart barChart = findViewById(R.id.bar_chart);
         barChart.setMaxValue(100);
